ea7f3a8930c9da1665d37b8e227b323be2ecc8cd
Ivan Vorobei <hello@ivanvorobei.by>
2021-05-26 21:08:08 +0300

    Updated docs.

--- Sources/SPIndicator/Protocols/SPIndicatorIconAnimatable.swift
+++ Sources/SPIndicator/Protocols/SPIndicatorIconAnimatable.swift
@@ -21,7 +21,13 @@
 
 import UIKit
 
+/**
+ SPIndicator: Protocol for animatable views.
+ */
 public protocol SPIndicatorIconAnimatable {
     
+    /**
+     SPIndicator: Shoud call when need start animation.
+     */
     func animate()
 }
